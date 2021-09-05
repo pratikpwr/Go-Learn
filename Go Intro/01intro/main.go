63f27d1b31e1bca02eb5568149c58e2a7cf44d4f
pratikpwr <pratiksatishpawar@gmail.com>
2021-09-05 15:09:45 +0530

    initial commit

---
+++ Go Intro/01intro/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+// go run filename.go -- compile and runs the program
+// go build filename.go -- only compile and build exe file
