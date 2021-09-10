520f473b035870d935308dde332205f0abe1783a
pratikpwr <pratiksatishpawar@gmail.com>
2021-09-10 23:21:55 +0530

    struct complete

--- GoLang/07Structs/main.go
+++ GoLang/07Structs/main.go
@@ -10,10 +10,14 @@ func main() {
 	lastName := getUserData("Enter your last name: ")
 	birthdate := getUserData("Enter your birthdate (DD/MM/YYYY): ")
 
-	var newUser User = CreateUser(firstName, lastName, birthdate)
+	// var newUser User = CreateUser(firstName, lastName, birthdate)
+	var newUserUsingPointer User = *CreateUserUsingPointer(firstName, lastName, birthdate)
 
 	// fmt.Println(firstName, lastName, birthdate, created)
-	fmt.Println(newUser)
+	// fmt.Println(newUser)
+	// fmt.Println(newUserUsingPointer)
+	// outputUerData(&newUserUsingPointer)
+	newUserUsingPointer.outputData()
 }
 
 func getUserData(promptText string) (data string) {
--- GoLang/07Structs/user.go
+++ GoLang/07Structs/user.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // Struct in Go
 // by this way struct can be defined
@@ -11,6 +14,8 @@ type User struct {
 	createdAt time.Time
 }
 
+// It will create a new copy of variable
+// which will increase resourse consuption
 func CreateUser(firstName string, lastName string, birthDate string) (user User) {
 	created := time.Now()
 
@@ -21,4 +26,32 @@ func CreateUser(firstName string, lastName string, birthDate string) (user User)
 		createdAt: created,
 	}
 	return
+}
+
+// using the pointers to create User which will save space
+// it will use same copy of  variable
+func CreateUserUsingPointer(firstName string, lastName string, birthDate string) *User {
+	created := time.Now()
+
+	user := User{
+		firstname: firstName,
+		lastname:  lastName,
+		birthdate: birthDate,
+		createdAt: created,
+	}
+	return &user
+}
+
+// to connect a function to struct and convert it into method
+// we use receiver argument as struct
+// we can use these methods on objects like newUser.OutputData()
+
+func (user User) outputData() {
+	fmt.Printf("My name is %v %v, I am born on %v.", user.firstname, user.lastname, user.birthdate)
+}
+
+func outputUerData(user *User) {
+	// go automatically dereferences pointer structs
+	// else we had to do like (*user).firstname
+	fmt.Printf("My name is %v %v, I am born on %v.", user.firstname, user.lastname, user.birthdate)
 }
